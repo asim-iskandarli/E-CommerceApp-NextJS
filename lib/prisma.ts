392d9199aa99278354900310ec156a7e75370763
asim-iskandarli <asim.iskandarli@gmail.com>
2024-09-09 00:54:52 +0400

    deploy 1.2

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,15 +1,9 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "@prisma/client";
+ 
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+ 
+const prisma = globalForPrisma.prisma || new PrismaClient();
+ 
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
 
-const prismaClientSingleton = () => {
-  return new PrismaClient()
-}
-
-declare const globalThis: {
-  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
-} & typeof global;
-
-const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
-
-export default prisma
-
-if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma
+export default prisma;
